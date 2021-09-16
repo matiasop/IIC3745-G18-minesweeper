d56fd311142038f9d32f4260aad7de1ba1eb3635
matiasop <movalle4@uc.cl>
2021-09-15 22:14:24 -0300

    fail test

--- test/example.rb
+++ test/example.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'test/unit'
 
 class ExampleTest < Test::Unit::TestCase
   def test_example
     # Hice este test para probar si funciona el github action
     # que corre los test. Hay que eliminarlo después.
-    assert_true(true)
+    assert_true(false)
   end
 end
