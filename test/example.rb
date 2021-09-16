77c3a5c0203d21024234850cb53db0a5fc58a8b2
matiasop <movalle4@uc.cl>
2021-09-15 22:05:45 -0300

    add example test

---
+++ test/example.rb
@@ -0,0 +1,9 @@
+require 'test/unit'
+
+class ExampleTest < Test::Unit::TestCase
+  def test_example
+    # Hice este test para probar si funciona el github action
+    # que corre los test. Hay que eliminarlo después.
+    assert_true(true)
+  end
+end
