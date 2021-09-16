a53f39f00de26dbe232445b8582d1ed9bef3f77c
matiasop <movalle4@uc.cl>
2021-09-15 22:16:20 -0300

    fix example test

--- test/example.rb
+++ test/example.rb
@@ -6,6 +6,6 @@ class ExampleTest < Test::Unit::TestCase
   def test_example
     # Hice este test para probar si funciona el github action
     # que corre los test. Hay que eliminarlo después.
-    assert_true(false)
+    assert_true(true)
   end
 end
