ceafda6d9f191fb56ea27eaea5f4d03c26a2df33
Alejandra Uribe <anuribe@uc.cl>
2021-09-30 10:20:44 -0300

    Rubocop autocorrect

--- lib/game_match.rb
+++ lib/game_match.rb
@@ -1,39 +1,37 @@
+# frozen_string_literal: true
+
 require_relative 'user_interface'
 require_relative 'board'
 
 class GameMatch
-    
-    def initialize(match_board)
-        @match_board = match_board
-        @game_over = false
-        @win = false
+  def initialize(match_board)
+    @match_board = match_board
+    @game_over = false
+    @win = false
+  end
+
+  def play_turn
+    command = UserInterface.receive_command
+    if command['action'] == 'exit'
+      exit
+    else
+      result = @match_board.apply_action_to_block(command['action'], command['row'], command['col'])
+      if !result
+        @game_over = true
+      elsif @match_board.played_blocks == @match_board.win_condition
+        @win = true
+      end
     end
+  end
 
-    def play_turn
-        command = UserInterface::receive_command
-        if command["action"] == "exit"
-            exit()
-        else
-            result = @match_board.apply_action_to_block(command["action"], command["row"], command["col"])
-            if !result
-                @game_over = true
-            else
-                if @match_board.played_blocks == @match_board.win_condition
-                    @win = true
-                end
-            end
-        end
-    end 
-
-    def do_match
-        @match_board.populate_blocks
-        @match_board.mark_contiguous
-        @match_board.do_blocks_counting
-        while !@game_over && !@win do
-            UserInterface::show_board(@match_board)
-            play_turn
-        end
-        UserInterface::show_results(@match_board, @win)
+  def do_match
+    @match_board.populate_blocks
+    @match_board.mark_contiguous
+    @match_board.do_blocks_counting
+    while !@game_over && !@win
+      UserInterface.show_board(@match_board)
+      play_turn
     end
-
+    UserInterface.show_results(@match_board, @win)
+  end
 end
