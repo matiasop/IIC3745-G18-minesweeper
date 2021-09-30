70c5910b6329645fbdb6ccca038331270d6694a6
Alejandra Uribe <anuribe@uc.cl>
2021-09-30 10:08:29 -0300

    Rubocop autocorrect

--- lib/board_block.rb
+++ lib/board_block.rb
@@ -1,51 +1,45 @@
+# frozen_string_literal: true
+
+# Clase que representa cada bloque del tablero y se encarga de su lógica
 class BoardBlock
-    NEIGHBORS = %w[N NE E SE S SW W NW]
-    attr_accessor :parent_board, :n_contiguous_bombs, :contiguous_zero_blocks, :played, :flagged, :contiguous
-    attr_reader :bomb
+  NEIGHBORS = %w[N NE E SE S SW W NW].freeze
+  attr_accessor :parent_board, :n_contiguous_bombs, :contiguous_zero_blocks, :played, :flagged, :contiguous
+  attr_reader :bomb
 
-    def initialize(parent_board, bomb)
-        @parent_board = parent_board
-        @bomb = bomb
-        @n_contiguous_bombs = 0
-        @contiguous_zero_blocks = []
-        @played = false
-        @flagged = false
-        #Using cardinal coordinates
-        @contiguous = NEIGHBORS.inject({}){|hash, key| hash[key] = nil; hash}
+  def initialize(parent_board, bomb)
+    @parent_board = parent_board
+    @bomb = bomb
+    @n_contiguous_bombs = 0
+    @contiguous_zero_blocks = []
+    @played = false
+    @flagged = false
+    # Using cardinal coordinates
+    @contiguous = NEIGHBORS.each_with_object({}) do |key, hash|
+      hash[key] = nil
     end
+  end
 
-    def count_contiguous_bombs
-        @contiguous.each do |key, value|
-            if !value.nil?
-                if value.bomb
-                    @n_contiguous_bombs += 1
-                end 
-            end
-        end
+  def count_contiguous_bombs
+    @contiguous.each do |_key, value|
+      @n_contiguous_bombs += 1 if !value.nil? && value.bomb
     end
+  end
 
-    def count_contiguous_zero_blocks
-        @contiguous.each do |key, value|
-            if !value.nil?
-                if value.n_contiguous_bombs == 0
-                    @contiguous_zero_blocks.push(value)
-                end 
-            end
-        end
+  def count_contiguous_zero_blocks
+    @contiguous.each do |_key, value|
+      @contiguous_zero_blocks.push(value) if !value.nil? && value.n_contiguous_bombs.zero?
     end
+  end
 
-    def play_block(board)
-        if @played || @flagged
-            return
-        end
-        @played = true
-        board.played_blocks += 1
-        if @n_contiguous_bombs == 0
-            @contiguous.each do |key, value|
-                value.play_block(board)
-            end 
-        end
+  def play_block(board)
+    return if @played || @flagged
+
+    @played = true
+    board.played_blocks += 1
+    if @n_contiguous_bombs.zero?
+      @contiguous.each do |_key, value|
+        value.play_block(board)
+      end
     end
+  end
 end
-                
-
